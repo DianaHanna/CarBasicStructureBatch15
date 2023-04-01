968486c74e5c0fb92593b78ff4173064b6bdec9c
DianaHanna <shehata.dianan@gmail.com>
2023-04-01 19:08:04 -0400

    Updated the main code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BasicStructure audi =new BasicStructure();
+        audi.color="balck";
+        audi.model="A3";
+        audi.power="5000cc";
+
+        Engine X =new Engine();
+        X.cylinders="8";
+        X.size="1234cc";
+        X.weight="6453k";
     }
 }
